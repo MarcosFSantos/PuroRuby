5ed6d4b512206ba0bbe212360f02c026836a0db3
MarcosFSantos <marcos.franca1673@gmail.com>
2024-12-31 11:59:25 -0300

    feat: Encapsular codigo em classe Server

--- main.rb
+++ main.rb
@@ -1,32 +1,4 @@
-require 'socket'
+require_relative 'server'
 
-server = TCPServer.new(5000)
-
-TIMEOUT = 10
-MAX_REQUESTS = 100
-
-# Aceitando conexões enquanto o loop estiver sendo executado
-loop do
-    # Aceita conexões simultâneas
-    Thread.new(server.accept) do |socket|
-        begin
-            request_count = 0
-            loop do
-                # Espera a leitura do socket por TIMEOUT segundos
-                ready = IO.select([socket], nil, nil, TIMEOUT)
-                break unless ready
-                # Responde mensagem do cliente no socket
-                line = socket.gets
-                puts "O cliente disse: #{line}"
-                socket.puts "Você disse: #{line}"
-                # Conta as requisições
-                request_count += 1
-                break if request_count >= MAX_REQUESTS
-            end
-        rescue => e
-            puts "Erro: #{e}"
-        ensure
-            socket.close
-        end
-    end
-end
+server = Server.new(5000)
+server.start
