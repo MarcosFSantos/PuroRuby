d765b05dd543925a09448c4d38ac8bd3aa42e5b7
MarcosFSantos <marcos.franca1673@gmail.com>
2024-12-30 22:55:31 -0300

    feat: Adicionar limite de tempo de inatividade no servidor

--- main.rb
+++ main.rb
@@ -7,11 +7,17 @@ loop do
     # Aceita conexões simultâneas
     Thread.new(server.accept) do |socket|
         begin
-            # Responde mensagens do cliente no socket
-            while line = socket.gets
+            TIMEOUT = 10
+            loop do
+                # Espera a leitura do socket por TIMEOUT segundos
+                ready = IO.select([socket], nil, nil, TIMEOUT)
+                break unless ready
+                # Responde mensagem do cliente no socket
+                line = socket.gets
                 puts "O cliente disse: #{line}"
                 socket.puts "Você disse: #{line}"
             end
+            socket.close
         rescue => e
             puts "Erro: #{e}"
         end
