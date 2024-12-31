c9be0aefae8c90e9e252ecd411dc8b3ba6e1fac4
MarcosFSantos <marcos.franca1673@gmail.com>
2024-12-30 21:44:35 -0300

    feat: Adicionar tratamento de erros no servidor

--- main.rb
+++ main.rb
@@ -6,10 +6,14 @@ server = TCPServer.new(5000)
 loop do
     # Aceita conexões simultâneas
     Thread.new(server.accept) do |socket|
-        # Responde mensagens do cliente no socket
-        while line = socket.gets
-            puts "O cliente disse: #{line}"
-            socket.puts "Você disse: #{line}"
+        begin
+            # Responde mensagens do cliente no socket
+            while line = socket.gets
+                puts "O cliente disse: #{line}"
+                socket.puts "Você disse: #{line}"
+            end
+        rescue => e
+            puts "Erro: #{e}"
         end
     end
 end
