ad364d7c61ac5a157d7d8e0481c726fdf23f60ae
MarcosFSantos <marcos.franca1673@gmail.com>
2025-01-02 17:19:45 -0300

    feat: Refatorar código do servidor TCP

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative 'server'
 
 server = Server.new(5000)
+trap('INT') { server.shutdown } # Encerra o servidor com CTRL+C
 server.start
--- server.rb
+++ server.rb
@@ -7,26 +7,40 @@ class Server
     end
 
     def start
-        # Aceitando conexões enquanto o loop estiver sendo executado
+        puts "Servidor iniciado"
+        # Aceita conexões enquanto o loop estiver sendo executado
         loop do
             # Aceita conexões simultâneas
-            Thread.new(@server.accept) do |socket|
-                begin
-                    loop do
-                        # Espera a leitura do socket por TIMEOUT segundos
-                        ready = IO.select([socket], nil, nil, @timeout)
-                        break unless ready
-                        # Responde mensagem do cliente no socket
-                        line = socket.gets
-                        puts "O cliente disse: #{line}"
-                        socket.puts "Você disse: #{line}"
-                    end
-                rescue => e
-                    puts "Erro: #{e}"
-                ensure
-                    socket.close
-                end
-            end
+            Thread.new(@server.accept) { |socket| handle_connection(socket) }
         end
+    end
+
+    def handle_connection(socket)
+        puts "Conexão estabelecida"
+        begin
+            handle_client(socket)
+        rescue => e
+            puts "Erro: #{e}"
+        ensure
+            puts "Conexão encerrada"
+            socket.close
+        end
+    end
+
+    def handle_client(socket)
+        loop do
+            # Espera a leitura do socket por TIMEOUT segundos
+            ready = IO.select([socket], nil, nil, @timeout)
+            break unless ready
+            # Responde mensagem do cliente no socket
+            line = socket.gets
+            p line
+            socket.puts "Você disse: #{line}"
+        end
+    end
+
+    def shutdown
+        @server.close
+        puts "Servidor encerrado"
     end
 end
