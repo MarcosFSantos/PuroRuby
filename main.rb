3572e1eaaeb96d51d0a4f80c77b7348f7f3bd0da
MarcosFSantos <marcos.franca1673@gmail.com>
2025-01-04 19:47:54 -0300

    feat: Permitir conexões esternas ao servidor
    Permitir o servidor para escutar em todos os endereços, necessário para a implementação do Docker.

--- main.rb
+++ main.rb
@@ -1,5 +1,5 @@
 require_relative 'server'
 
-server = Server.new("localhost", 5000, 4)
+server = Server.new('0.0.0.0', 5000, 4)
 trap('INT') { server.shutdown } # Encerra o servidor com CTRL+C
 server.start
